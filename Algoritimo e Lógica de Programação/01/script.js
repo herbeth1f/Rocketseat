e744e357bdbc0dc0fb54eac600e6bf831d048a4b
Herbeth <herbeth.henrique@gmail.com>
2022-12-22 08:58:28 -0300

    Aulas

---
+++ Algoritimo e Lógica de Programação/01/script.js
@@ -0,0 +1,8 @@
+/*
+ Encontre a solução ao problema
+
+ Pergunte o nome do usuário e escreva a mensagem;
+ "Olá , [Nome do Usuário]"
+*/
+let nome = prompt("Qual o seu nome ?");
+alert("Ola," + nome)
